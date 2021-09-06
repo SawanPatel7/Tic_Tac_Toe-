f49057a14c02bed6bdfb09dc4d6d126b16a1dc61
sawan patel <sawanp06@gmail.com>
2021-09-06 18:07:03 +0530

    minimax done

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ const wonText = document.querySelector(".won-text");
 const Replaybnt = document.querySelector(".result button");
 
 /// in minimax branch
-
+let boardbox = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+];
 window.onload = () => {// once the window loaded
     for (let i = 0; i < allbox.length; i++) {
         allbox[i].setAttribute("onclick", "clickbox(this)");
@@ -28,93 +32,116 @@ window.onload = () => {// once the window loaded
 
 }
 
+
 let playerXIcon = "fas fa-times",
     playerOIcon = "far fa-circle",
     playerSign = "X",
     rungame = true;
 
 function clickbox(element) { // when player click any box
-  if(rungame)
-  {
-    if (Move.classList.contains("PlayerO")) {
-        playerSign = "O";
-        element.innerHTML = `<i class="${playerOIcon}"></i>`;
-        Move.classList.remove("active");
-        element.setAttribute("id",playerSign);
-    } else {
-        element.innerHTML = `<i class="${playerXIcon}"></i>`;
-        Move.classList.add("active");
-        element.setAttribute("id",playerSign);
+    if (rungame) {
+        if (Move.classList.contains("PlayerO")) {
+            playerSign = "O";
+            element.innerHTML = `<i class="${playerOIcon}"></i>`;
+            let curX = Math.floor(element.classList[1] / 3);
+            let curY = element.classList[1] % 3;
+            boardbox[curX][curY] = "O";
+            Move.classList.remove("active");
+        } else {
+            element.innerHTML = `<i class="${playerXIcon}"></i>`;
+            let curX = Math.floor(element.classList[1] / 3);
+            let curY = element.classList[1] % 3;
+            boardbox[curX][curY] = "X";
+            Move.classList.add("active");
+        }
+        element.style.pointerEvents = "none";
+        Playboard.style.pointerEvents = "none";
+        selectWinner();
+        let randomTimeDelay = (Math.random() * 500 + 500).toFixed();
+        setTimeout(function () {
+            game(rungame);
+        }, randomTimeDelay);
     }
-    element.style.pointerEvents = "none";
-    Playboard.style.pointerEvents="none";
-    selectWinner();
-    let randomTimeDelay=(Math.random()*500+500).toFixed();
-    setTimeout(function(){
-        game(rungame);
-    },randomTimeDelay);
-  }
 }
 
 
 //main function
 function game(rungame) {   // computer move
-    if(rungame){
-    let array = [];
-    for (let i = 0; i < allbox.length; i++) {
-        const ele = allbox[i];
-        if (ele.childElementCount == 0) {
-            array.push(i);
+    if (rungame) {
+        let array = [];
+        for (let i = 0; i < allbox.length; i++) {
+            const ele = allbox[i];
+            if (ele.childElementCount == 0) {
+                array.push(i);
+            }
         }
-    }
-    let randomBox = array[Math.floor(Math.random() * array.length)];
-    if (array.length > 0) {
-        if (Move.classList.contains("PlayerO")) {
-            playerSign="X";
-            allbox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-            allbox[randomBox].setAttribute("id",playerSign);
-            Move.classList.add("active");
-        } else {
-            playerSign="O";
-            allbox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-            allbox[randomBox].setAttribute("id",playerSign);
-            Move.classList.remove("active");
+        // let randomBox = array[Math.floor(Math.random() * array.length)];
+        let bestPosition = bestmove();
+        let element = allbox[bestPosition];
+        if (array.length > 0) {
+            if (Move.classList.contains("PlayerO")) {
+                playerSign = "X";
+                element.innerHTML = `<i class="${playerXIcon}"></i>`;
+                let curX = Math.floor(element.classList[1] / 3);
+                let curY = element.classList[1] % 3;
+                boardbox[curX][curY] = "X";
+                Move.classList.add("active");
+            } else {
+                playerSign = "O";
+                element.innerHTML = `<i class="${playerOIcon}"></i>`;
+                let curX = Math.floor(element.classList[1] / 3);
+                let curY = element.classList[1] % 3;
+                boardbox[curX][curY] = "O";
+                Move.classList.remove("active");
 
+            }
+            selectWinner();
         }
-        selectWinner();
+        console.log(boardbox);
+        element.style.pointerEvents = "none";
+        Playboard.style.pointerEvents = "auto";
+        playerSign = "X";
     }
-    allbox[randomBox].style.pointerEvents="none";
-    Playboard.style.pointerEvents="auto";
-    playerSign="X";
-}
 
 }
 
-function getIdVal(classname){
-    return document.querySelector(".box-" + classname).id;
+function getIdVal(classname) {
+    let curX = Math.floor((classname) / 3);
+    let curY = Math.floor((classname) % 3);
+    return boardbox[curX][curY];
 }
 
-function checkIdSign(val1, val2, val3, sign){ 
-    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
-        return true;
+function checkWinner(Sign) {
+    for (let i = 0; i < 3; i++) {
+        if (boardbox[i][0] == Sign && boardbox[i][1] == Sign && boardbox[i][2] == Sign) return true;
+        if (boardbox[0][i] == Sign && boardbox[1][i] == Sign && boardbox[2][i] == Sign) return true;
     }
+    if (boardbox[0][0] == Sign && boardbox[1][1] == Sign && boardbox[2][2] == Sign) return true;
+    if (boardbox[0][2] == Sign && boardbox[1][1] == Sign && boardbox[2][0] == Sign) return true;
+    return false;
 }
 
-function selectWinner()
-{
-    if(checkIdSign(1,2,3,playerSign)||checkIdSign(4,5,6,playerSign)||checkIdSign(7,8,9,playerSign)||checkIdSign(1,4,7,playerSign)||checkIdSign(2,5,8,playerSign)||checkIdSign(3,6,9,playerSign)||checkIdSign(1,5,9,playerSign)||checkIdSign(3,5,7,playerSign)){
-        rungame=false;
+function checkDraw() {
+    for (let i = 0; i < 3; i++) {
+        if (boardbox[0][i] == "" || boardbox[1][i] == "" || boardbox[2][i] == "") return false;
+    }
+    return true;
+}
+
+function selectWinner() {
+    if (checkWinner(playerSign)) {
+        rungame = false;
         // game(rungame);
-        setTimeout(()=>{
+        setTimeout(() => {
             ResultBox.classList.add("show");
             Playboard.classList.remove("show");
         }, 1000);
-        wonText.innerHTML = "Player "+ playerSign +" won the game!";
-    }else{
-        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
+        wonText.innerHTML = "Player " + playerSign + " won the game!";
+    } else {
+        if (checkDraw()) {
             rungame = false;
             // game(rungame);
-            setTimeout(()=>{
+            setTimeout(() => {
                 ResultBox.classList.add("show");
                 Playboard.classList.remove("show");
             }, 700);
@@ -123,7 +150,90 @@ function selectWinner()
     }
 }
 
+/////////////minimax algorithm section
 
-Replaybnt.addEventListener('click',()=>{
+
+let Ai = "O";
+let Player="X";
+
+function bestmove()
+{
+    Ai="O";
+    Player="X";
+    if(Move.classList.contains("PlayerO"))
+    {
+        Ai="X";
+        Player="O";
+    }
+    let maxScore= -10000;
+    let bestPosition;
+    for(let i=0;i<3;i++)
+    {
+       for(let j=0;j<3;j++)
+       {
+          if(boardbox[i][j]=="")
+          {
+              boardbox[i][j]=Ai;
+              let curScore=minSearch();
+              if(curScore>maxScore)
+              {
+                 maxScore=curScore;
+                 bestPosition=3*i+j;
+              }
+              boardbox[i][j]="";
+          }
+       }
+    }
+    return bestPosition;
+}
+
+
+function minSearch(){
+    if(checkWinner(Ai))return 10;
+    if(checkWinner(Player))return -10;
+    if(checkDraw())return 0;
+    let minScore= 10000;
+    for(let i=0;i<3;i++)
+    {
+        for(let j=0;j<3;j++)
+        {
+            if(boardbox[i][j]==""){
+                boardbox[i][j]=Player;
+                let curScore=maxSearch();
+                minScore=Math.min(minScore,curScore);
+                boardbox[i][j]="";
+            }
+        }
+    }
+    return minScore;
+}
+
+function maxSearch()
+{
+    if(checkWinner(Ai))return 10;
+    if(checkWinner(Player))return -10;
+    if(checkDraw())return 0;
+    let maxScore= -10000;
+    for(let i=0;i<3;i++)
+    {
+       for(let j=0;j<3;j++)
+       {
+          if(boardbox[i][j]=="")
+          {
+              boardbox[i][j]=Ai;
+              let curScore=minSearch();
+              maxScore=Math.max(curScore,maxScore);
+              boardbox[i][j]="";
+          }
+       }
+    }
+    return maxScore;
+    
+}
+
+
+console.log(boardbox);
+
+Replaybnt.addEventListener('click', () => {
     window.location.reload();
 })
